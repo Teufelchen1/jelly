87a065126b11f8db5890e27b61ab5f26a6b7ea58
Bennet Blischke <bennet.blischke@haw-hamburg.de>
2025-04-15 22:32:53 +0200

    💩: Autocompletion

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -9,7 +9,6 @@ use ratatui::text::Text;
 use slipmux::encode_configuration;
 use tui_widgets::scrollview::ScrollViewState;
 
-use crate::app::commands::Command;
 use crate::app::commands::CommandLibrary;
 use crate::events::Event;
 
@@ -17,14 +16,14 @@ mod commands;
 mod handler;
 mod tui;
 
-pub struct App<'a> {
+pub struct App<'text> {
     event_sender: Sender<Event>,
     write_port: Option<String>,
     configuration_requests: Vec<CoapRequest<String>>,
     configuration_packets: Vec<Packet>,
     configuration_scroll_state: ScrollViewState,
     configuration_scroll_follow: bool,
-    pub diagnostic_messages: Text<'a>,
+    diagnostic_messages: Text<'text>,
     diagnostic_messages_scroll_state: ScrollViewState,
     diagnostic_messages_scroll_position: usize,
     diagnostic_messages_scroll_follow: bool,
@@ -89,7 +88,61 @@ impl App<'_> {
             .unwrap();
     }
 
-    fn suggest_command(&self) -> Option<&Command> {
-        self.known_commands.matching_prefix_by_cmd(&self.user_input)
+    fn _suggest_command(&self) -> Option<(&str, &str)> {
+        let cmds = self.known_commands.matching_prefix_by_cmd(&self.user_input);
+        let mut matching_cutoff = self.user_input.len();
+
+        match cmds.len() {
+            0 => {
+                None
+            },
+            1 => {
+                Some((&cmds[0].cmd, &cmds[0].description))
+            },
+            _ => {
+                let first = &cmds[0].cmd;
+                while matching_cutoff+1 < first.len() {
+                    let (cmd_name, _rest) = first.split_at(matching_cutoff+1);
+                    if !cmds.eq(&self.known_commands.matching_prefix_by_cmd(cmd_name)) {
+                        break;
+                    }
+                    matching_cutoff += 1;
+                }
+                let suggestion = first.split_at(matching_cutoff).0;
+                Some((suggestion, "Foo"))
+            }
+        }
+
+
+        // if cmds.len() > 1 {
+        //     let first_ = self.known_commands.matching_prefix_by_cmd(&self.user_input).first();
+        //     if let Some(first) = first_ {
+        //         while matching_cutoff+1 < first.cmd.len() {
+        //             let cmd_name = first.cmd.split_at(matching_cutoff+1).0;
+        //             if !cmds.eq(&self.known_commands.matching_prefix_by_cmd(cmd_name)) {
+        //                 break;
+        //             }
+        //             matching_cutoff += 1;
+        //         }
+        //     }
+        // if cmds.len() == 0 {
+
+        // }
+        // } else if cmds.len() == 1 {
+        //     return Some(&cmds.pop().unwrap().cmd);
+        // } else {
+
+        // }
+
+
+        // let first_ = self.known_commands.matching_prefix_by_cmd(&self.user_input).pop();
+        // if let Some(first) = first_ {
+        //     let second_ = self.known_commands.matching_prefix_by_cmd(&self.user_input).pop();
+        //     if let Some(second) = second_ {
+
+        //         let (left, right) = first.cmd.split_at(self.user_input.len()+1);
+
+        //     }
+        // }
     }
 }
