82dfb4d2eff49697ba2c50c5a9ae544632021a00
Teufelchen1 <bennet.blischke@haw-hamburg.de>
2025-07-08 11:03:45 +0200

    🏗️ : Add timestamps to messages, add command log

--- src/app/tui.rs
+++ src/app/tui.rs
@@ -1,12 +1,7 @@
 use std::cmp::max;
 use std::env;
-use std::fmt::Write;
 use std::iter::zip;
 
-use coap_lite::CoapOption;
-use coap_lite::ContentFormat;
-use coap_lite::MessageClass;
-use coap_lite::Packet;
 use ratatui::layout::Alignment;
 use ratatui::layout::Constraint;
 use ratatui::layout::Direction;
@@ -30,12 +25,13 @@ use tui_widgets::scrollview::ScrollView;
 
 use crate::app::App;
 
-impl App<'_> {
+impl App {
     fn render_header_footer(&self, frame: &mut Frame, header_area: Rect, footer_area: Rect) {
         let tab_titles = [
             "Overview (F1)",
             "Diagnostic (F2)",
             "Configuration (F3)",
+            "Commands (F4)",
             "Help (F5)",
         ];
         let title = if frame.area().width < 100 {
@@ -45,7 +41,7 @@ impl App<'_> {
         };
 
         let title_area =
-            Layout::horizontal([Constraint::Length(59), Constraint::Fill(1)]).split(header_area);
+            Layout::horizontal([Constraint::Length(72), Constraint::Fill(1)]).split(header_area);
 
         frame.render_widget(
             Tabs::new(tab_titles)
@@ -173,6 +169,58 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
         frame.render_widget(border_block, area);
     }
 
+    fn render_commands(&mut self, frame: &mut Frame, area: Rect) {
+        let right_block_up = Block::bordered()
+            .border_style(Style::new().gray())
+            .title(vec![Span::from("Commands")])
+            .title_alignment(Alignment::Left);
+
+        let mut req_blocks = vec![];
+        let mut constrains = vec![];
+        let total_length: u16 = {
+            let mut sum = 0;
+            // temporay limitation to work around ratatui bug #1855
+            let start =
+                usize::try_from(max(i64::try_from(self.job_log.jobs.len()).unwrap() - 10, 0))
+                    .unwrap();
+            for job in &mut self.job_log.jobs[start..] {
+                let (size, para) = job.paragraph();
+                req_blocks.push(para);
+                sum += size;
+                constrains.push(Constraint::Length(size.try_into().unwrap()));
+            }
+            sum.try_into().unwrap_or(u16::MAX)
+        };
+
+        let width = if right_block_up.inner(area).height < total_length {
+            // Make room for the scroll bar
+            right_block_up.inner(area).width - 1
+        } else {
+            right_block_up.inner(area).width
+        };
+
+        if self.configuration_scroll_follow {
+            self.configuration_scroll_state.scroll_to_bottom();
+        }
+
+        let mut scroll_view = ScrollView::new(Size::new(width, total_length));
+        let buf = scroll_view.buf_mut();
+        let scroll_view_area = buf.area;
+        let areas: Vec<Rect> = Layout::vertical(constrains)
+            .split(scroll_view_area)
+            .to_vec();
+        for (a, req_b) in zip(areas, req_blocks) {
+            req_b.render(a, buf);
+        }
+
+        frame.render_stateful_widget(
+            scroll_view,
+            right_block_up.inner(area),
+            &mut self.configuration_scroll_state,
+        );
+        frame.render_widget(right_block_up, area);
+    }
+
     fn render_configuration_messages(&mut self, frame: &mut Frame, area: Rect) {
         let right_block_up = Block::bordered()
             .border_style(Style::new().gray())
@@ -185,35 +233,20 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
             let mut sum = 0;
             // temporay limitation to work around ratatui bug #1855
             let start = usize::try_from(max(
-                i64::try_from(self.configuration_requests.len()).unwrap() - 10,
+                i64::try_from(self.configuration_log.len()).unwrap() - 10,
                 0,
             ))
             .unwrap();
-            for req in &self.configuration_requests[start..] {
-                let block = Block::new()
-                    .borders(Borders::TOP | Borders::BOTTOM)
-                    .style(Style::new().gray())
-                    // Realistically, there is exactly one line in a request; long term, we might
-                    // want to use the rest too.
-                    .title(
-                        fmt_packet(&req.message)
-                            .lines
-                            .drain(..)
-                            .next()
-                            .unwrap_or_default(),
-                    )
-                    .title_alignment(Alignment::Left);
-                if let Some(resp) = &req.response {
-                    let text = fmt_packet(&resp.message);
-                    let linecount = text.lines.len();
-                    sum += linecount + 2;
-                    constrains.push(Constraint::Min((linecount + 2).try_into().unwrap()));
-                    req_blocks.push(Paragraph::new(text).block(block));
+            for req in &mut self.configuration_log[start..] {
+                let (size, para) = if matches!(self.current_tab, super::SelectedTab::Configuration)
+                {
+                    req.paragraph()
                 } else {
-                    req_blocks.push(Paragraph::new("Awaiting response").block(block));
-                    sum += 3;
-                    constrains.push(Constraint::Length(3));
-                }
+                    req.paragraph_short()
+                };
+                req_blocks.push(para);
+                sum += size;
+                constrains.push(Constraint::Length(size.try_into().unwrap()));
             }
             sum.try_into().unwrap_or(u16::MAX)
         };
@@ -304,12 +337,19 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
             .title_alignment(Alignment::Left);
 
         let content_width = left_block_up.inner(area).width;
-        let messages_hight = u16::try_from(self.diagnostic_messages.height()).unwrap_or(u16::MAX);
+
+        let (messages_height, paragraph) =
+            if matches!(self.current_tab, super::SelectedTab::Diagnostic) {
+                self.diagnostic_log.paragraph()
+            } else {
+                self.diagnostic_log.paragraph_short()
+            };
+        let messages_height = messages_height.try_into().unwrap_or(u16::MAX);
 
         let mut scroll_view = ScrollView::new(Size::new(
             // Make room for the scroll bar
             content_width - 1,
-            messages_hight,
+            messages_height,
         ));
 
         if self.diagnostic_messages_scroll_follow {
@@ -317,8 +357,47 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
         }
 
         scroll_view.render_widget(
-            Paragraph::new(self.diagnostic_messages.clone()),
-            Rect::new(0, 0, content_width - 1, messages_hight),
+            paragraph,
+            Rect::new(0, 0, content_width - 1, messages_height),
+        );
+
+        frame.render_stateful_widget(
+            scroll_view,
+            left_block_up.inner(area),
+            &mut self.diagnostic_messages_scroll_state,
+        );
+        frame.render_widget(left_block_up, area);
+    }
+
+    fn render_overall_messages(&mut self, frame: &mut Frame, area: Rect) {
+        let left_block_up = Block::bordered()
+            .border_style(Style::new().gray())
+            .title(vec![Span::from("Diagnostic Messages")])
+            .title_alignment(Alignment::Left);
+
+        let content_width = left_block_up.inner(area).width;
+
+        let (messages_height, paragraph) =
+            if matches!(self.current_tab, super::SelectedTab::Diagnostic) {
+                self.overall_log.paragraph()
+            } else {
+                self.overall_log.paragraph_short()
+            };
+        let messages_height = messages_height.try_into().unwrap_or(u16::MAX);
+
+        let mut scroll_view = ScrollView::new(Size::new(
+            // Make room for the scroll bar
+            content_width - 1,
+            messages_height,
+        ));
+
+        if self.diagnostic_messages_scroll_follow {
+            self.diagnostic_messages_scroll_state.scroll_to_bottom();
+        }
+
+        scroll_view.render_widget(
+            paragraph,
+            Rect::new(0, 0, content_width - 1, messages_height),
         );
 
         frame.render_stateful_widget(
@@ -390,7 +469,7 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
                 let left_chunk_lower = left_chunks[1];
 
                 self.render_configuration_messages(frame, right_chunk_upper);
-                self.render_diagnostic_messages(frame, left_chunk_upper);
+                self.render_overall_messages(frame, left_chunk_upper);
                 self.render_configuration_overview(frame, right_chunk_lower);
                 self.render_user_input(frame, left_chunk_lower);
             }
@@ -418,6 +497,18 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
                 self.render_configuration_messages(frame, chunk_upper);
                 self.render_user_input(frame, chunk_lower);
             }
+            super::SelectedTab::Commands => {
+                let chunks = Layout::default()
+                    .direction(Direction::Vertical)
+                    .constraints([Constraint::Fill(1), Constraint::Length(3)].as_ref())
+                    .split(main_area);
+
+                let chunk_upper = chunks[0];
+                let chunk_lower = chunks[1];
+
+                self.render_commands(frame, chunk_upper);
+                self.render_user_input(frame, chunk_lower);
+            }
             super::SelectedTab::Help => {
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
@@ -432,73 +523,4 @@ If a command doesn't offer binary export, the `%>` will automatically downgrade 
             }
         }
     }
-}
-
-fn fmt_packet(packet: &Packet) -> Text {
-    // When writing to a String `write!` will never fail.
-    // Therefore the Result is ignored with `_ = write!()`.
-    let mut out = String::new();
-    match packet.header.code {
-        MessageClass::Empty => _ = write!(out, "Empty"),
-        MessageClass::Request(rtype) => {
-            _ = write!(out, " ← Req({rtype:?} ");
-            if let Some(option_list) = packet.get_option(CoapOption::UriPath) {
-                for option in option_list {
-                    _ = write!(out, "/{}", String::from_utf8_lossy(option));
-                }
-            } else {
-                _ = write!(out, "/");
-            }
-            _ = write!(
-                out,
-                ")[0x{:04x}]",
-                u16::from_le_bytes(packet.get_token().try_into().unwrap_or([0xff, 0xff]))
-            );
-        }
-        MessageClass::Response(rtype) => {
-            let responsestyle = match rtype {
-                // FIXME: Use classification instead
-                coap_lite::ResponseType::Content => Style::new().green(),
-                coap_lite::ResponseType::NotFound => Style::new().red(),
-                _ => Style::new(),
-            };
-
-            let cf = packet.get_content_format();
-            let payload_formatted = match (cf, &packet.payload) {
-                (Some(ContentFormat::ApplicationLinkFormat), payload) => {
-                    String::from_utf8_lossy(payload).replace(",<", ",\n  <")
-                }
-                (Some(ContentFormat::TextPlain), payload) => {
-                    String::from_utf8_lossy(payload).to_string()
-                }
-                // this is a cheap-in-terms-of-dependencies hex formatting; `aa bb cc` would be
-                // prettier than `[aa, bb, cc]`, but needs extra dependencies.
-                (Some(ContentFormat::ApplicationCBOR), payload) => {
-                    cbor_edn::StandaloneItem::from_cbor(payload).map_or_else(
-                        |e| format!("Parsing error {e}, content {payload:02x?}"),
-                        |c| c.serialize(),
-                    )
-                }
-                (_, payload) => format!("{payload:02x?}"),
-            };
-            let slash_cf = cf.map(|c| format!("/{c:?}")).unwrap_or_default();
-            let mut out = Text::default();
-            let mut firstline = format!(
-                " → Res({rtype:?}{slash_cf})[0x{:04x}]",
-                u16::from_le_bytes(packet.get_token().try_into().unwrap_or([0xff, 0xff])),
-            );
-            let tail = if packet.payload.is_empty() {
-                "  Empty Payload".to_owned()
-            } else {
-                _ = write!(firstline, ": {} bytes", packet.payload.len());
-                format!("  {payload_formatted}")
-            };
-            out.lines.push(Line::styled(firstline, responsestyle));
-            // FIXME: Is there no easier way to create an iterator of Line from a String?
-            out.lines.extend(Text::from(tail).lines);
-            return out;
-        }
-        MessageClass::Reserved(_) => _ = write!(out, "Reserved"),
-    }
-    Text::from(out)
 }
